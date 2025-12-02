5d824533ec6808a8787232960e97ad747ec65d23
Jun Gan <junyangan@hotmail.com>
2025-12-02 02:16:35 -0500

    updated and preparing for other api calls, created a model schema separate file

--- pages/api/games/[id].js
+++ pages/api/games/[id].js
@@ -1,72 +1,10 @@
 import mongoose from 'mongoose';
 import dbConnect from '../../../lib/mongo/dbConnect';
-
-const GameSchema = new mongoose.Schema({
-  game_name: {
-    type: String,
-    required: [true, 'Please provide a name for this game.'],
-    maxlength: [100, 'Game name cannot be more than 100 characters'],
-  },
-  review: {
-    type: String,
-    required: false,
-  },
-  last_played: {
-    type: Date,
-    required: false,
-  },
-  rating: {
-    type: Number,
-    required: false,
-    min: 1,
-    max: 5,
-  },
-  number_of_achievements: {
-    type: Number,
-    required: false,
-    min: 0,
-  },
-  number_achieved: {
-    type: Number,
-    required: false,
-    min: 0,
-  },
-  percentage_completed: {
-    type: Number,
-    required: false,
-    min: 0,
-    max: 100,
-  },
-  hours_played: {
-    type: Number,
-    required: false,
-    min: 0,
-  },
-  finished_game: {
-    type: String,
-    required: false,
-  },
-  isFinished: {
-    type: Boolean,
-    required: false,
-    default: false,
-  },
-  id: {
-    type: Number,
-    required: true,
-    unique: true,
-  }
-});
-
-// Check if the model already exists before creating it (Next.js requirement)
-const Game = mongoose.models.Game || mongoose.model('Game', GameSchema);
-GameSchema.set('id', false); 
-GameSchema.set('toJSON', { virtuals: false });
-GameSchema.set('toObject', { virtuals: false });
+import Game from '../../../models/Game'
 
 // --- API Route Handler ---
 
-export default async function getGameById(req, res) {
+export default async function apiHandler(req, res) {
   await dbConnect();
   
   const { 
